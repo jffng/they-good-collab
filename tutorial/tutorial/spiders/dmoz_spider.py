c1c10be02300256fb3fb34d751e3bd006ee14bc4
Jeff Ong <jonger4@gmail.com>
2014-06-02 20:38:20 -0400

    working spider

--- tutorial/tutorial/spiders/dmoz_spider.py
+++ tutorial/tutorial/spiders/dmoz_spider.py
@@ -1,4 +1,7 @@
 from scrapy.spider import Spider
+from scrapy.selector import Selector
+
+from tutorial.items import DmozItem
 
 class DmozSpider(Spider):
 	name = "dmoz"
@@ -9,5 +12,18 @@ class DmozSpider(Spider):
 	]
 
 	def parse(self, response):
-		filename = response.url.split("/")[-2]
-		open(filename, 'wb').write(response.body)
+		sel = Selector(response)
+		sites = sel.xpath('//ul/li')
+		items = []
+
+		for site in sites:
+			item = DmozItem()
+			item['title'] = site.xpath('a/text()').extract()
+			item['link'] = site.xpath('a/@href').extract()
+			item['desc'] = site.xpath('text()').extract()
+			items.append(item)
+
+		return items
+
+		# filename = response.url.split("/")[-2]
+		# open(filename, 'wb').write(response.body)
